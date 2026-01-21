182766c2f657abc6e8be8f01d39c5e99325917ab
MauroGutMB <mgmbarros2006@gmail.com>
2026-01-21 10:59:18 -0300

    algo de java

---
+++ JAVA_dir/APS/05_01/Enfermaria/Paciente.java
@@ -0,0 +1,13 @@
+/**
+ * Paciente
+ * \Atributos: nome, idade, comorbidade, cidadeResidencia
+ */
+public class Paciente {
+
+  private String nome;
+  private Integer idade;
+  private String comorbidade;
+  private String cidadeResidencia;
+
+
+}
